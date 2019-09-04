5f99c292f525b51bb14a4f1a8538129503c1b0d0
dimansolo <dimansolo84@gmail.com>
2019-09-04 20:24:10 +0300

    included express

--- index.js
+++ index.js
@@ -1,74 +1,116 @@
-
-let http = require("http");
-// let {getUsers,addUser} = require('./repository')
-
-let {usersController} = require('./usersController')
+const express = require('express');
 
 
-// let users = [
-//     {"id": 1, "name": "vovchik" },
-//     {"id": 2, "name": "vovchik3" },
-// ]
+const users = require("./users-Router")
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
 
 
 
 
-let cors = (req, res) => {
+app.use(cors());
 
-    // Set CORS headers
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Request-Method', '*');
-res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-res.setHeader('Access-Control-Allow-Headers', '*');
-if ( req.method === 'OPTIONS' ) {
-    res.writeHead(200);
-    res.end();
-    return(tue);
-}
-return(false);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-}
-
-let server = http.createServer((req, res) => {
-
-   if(cors(req,res)) return;
+app.use('/users', users);
 
 
-    console.log("some request")
 
-    switch(req.url) {
-        case '/users': 
 
-        usersController(req,res);
+app.get('/tasks', async (req, res) => {
+    res.send("TASKS");
+
+});
+
+
+app.use((req, res) => {
+    res.send(404)
+});
+
+
+
+
+app.listen(7743,  () => {
+    console.log('Example app listening on port 7743!');
+});
+
+
+
+
+
+
+// let http = require("http");
+// // let {getUsers,addUser} = require('./repository')
+
+// let {usersController} = require('./usersController')
+
+
+// // let users = [
+// //     {"id": 1, "name": "vovchik" },
+// //     {"id": 2, "name": "vovchik3" },
+// // ]
+
+
+
+
+// let cors = (req, res) => {
+
+//     // Set CORS headers
+// res.setHeader('Access-Control-Allow-Origin', '*');
+// res.setHeader('Access-Control-Request-Method', '*');
+// res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+// res.setHeader('Access-Control-Allow-Headers', '*');
+// if ( req.method === 'OPTIONS' ) {
+//     res.writeHead(200);
+//     res.end();
+//     return(tue);
+// }
+// return(false);
+
+// }
+
+// let server = http.createServer((req, res) => {
+
+//    if(cors(req,res)) return;
+
+
+//     console.log("some request")
+
+//     switch(req.url) {
+//         case '/users': 
+
+//         usersController(req,res);
            
-            // if(req.method === "POST") {
+//             // if(req.method === "POST") {
                 
 
-            //     addUser('IGNAT2')
-            //     res.write(JSON.stringify({success: true}))
-            // }
-            // else {
-            //     res.write(JSON.stringify(getUsers()))
-            // }
+//             //     addUser('IGNAT2')
+//             //     res.write(JSON.stringify({success: true}))
+//             // }
+//             // else {
+//             //     res.write(JSON.stringify(getUsers()))
+//             // }
 
         
-        break;
+//         break;
 
-        case '/lessons': {
-            res.write("TASKS")
-        }
-        break;
-        default:
-            res.write("PAGE IS NOT FOUND")
+//         case '/lessons': {
+//             res.write("TASKS")
+//         }
+//         break;
+//         default:
+//             res.write("PAGE IS NOT FOUND")
 
-    }
+//     }
 
-    // res.end()
+//     // res.end()
 
-}  )
+// }  )
 
-server.listen(7742)
-console.log(http)
+// server.listen(7742)
+// console.log(http)
 
 
 
--- repository.js
+++ repository.js
@@ -7,49 +7,99 @@
 
 
 
-let fs = require('fs');
+
+let {reedFile, writeFile} = require('./fs-utills')
 
 
 const addUser = async (name) => {
     let users = await getUsers();
     users.push({ name: name });
 
+    return writeFile ("users.json", users)
 
-
-    return new Promise((resolve, reject) => {
-        fs.writeFile("users.json", JSON.stringify(users), (err) => {
-            if(err) reject(err)
+    // return new Promise((resolve, reject) => {
+    //     fs.writeFile("users.json", JSON.stringify(users), (err) => {
+    //         if(err) reject(err)
             
-            resolve()
+    //         resolve()
             
            
-        } )
+    //     } )
 
 
-    })
+    // })
 
 
 }
 
 
 const getUsers = async() => {
-    // return users    
+    // return users   
+    
+    return reedFile("users.json")
 
-    return new Promise((resolve, reject) => {
-        fs.readFile("users.json", function (err, buf) {
-            if(err) {
-                reject(err)
-            }
-            else{
-            resolve(JSON.parse(buf.toString()));
-            }
-        })
+    // return new Promise((resolve, reject) => {
+    //     fs.readFile("users.json", function (err, buf) {
+    //         if(err) {
+    //             reject(err)
+    //         }
+    //         else{
+    //         resolve(JSON.parse(buf.toString()));
+    //         }
+    //     })
 
-    })
+    // })
 
 }
 
 
 exports.addUser = addUser;
 exports.getUsers = getUsers;
+
+
+// let fs = require('fs');
+
+
+// const addUser = async (name) => {
+//     let users = await getUsers();
+//     users.push({ name: name });
+
+
+
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile("users.json", JSON.stringify(users), (err) => {
+//             if(err) reject(err)
+            
+//             resolve()
+            
+           
+//         } )
+
+
+//     })
+
+
+// }
+
+
+// const getUsers = async() => {
+//     // return users    
+
+//     return new Promise((resolve, reject) => {
+//         fs.readFile("users.json", function (err, buf) {
+//             if(err) {
+//                 reject(err)
+//             }
+//             else{
+//             resolve(JSON.parse(buf.toString()));
+//             }
+//         })
+
+//     })
+
+// }
+
+
+// exports.addUser = addUser;
+// exports.getUsers = getUsers;
 
